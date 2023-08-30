5ec0acf5051fde88633b359e58ad0fa97a9155ed
Shilpajordan <Shilpa.nair@dci-student.org>
2023-08-30 14:16:08 +0200

    completed the task

---
+++ main.py
@@ -0,0 +1,47 @@
+### Task 1
+
+""" city = 'London'
+print(city)
+ """
+
+### Task 2
+
+""" city = 'berlin'
+city1 = city.capitalize()
+population = 3645000
+
+print(city1,population,sep=': ') """
+
+### Task 3
+""" city = 'london'
+city1 = city.capitalize()
+population = 9000000
+result = isinstance(city,str)
+
+print('City:',city1,'(',str(result),')')
+print('Population',population,sep=': ') """
+
+### Task 4
+
+""" text = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital."
+text1 = text.find('capital')
+
+print('capital',text.find('capital'),sep=': ') """
+
+
+### Task 5
+""" text = "Berlin straddles the banks of the Spree, which flows into the Havel (a tributary of the Elbe) in the western borough of Spandau."
+
+text1 = text.split(' ')
+list = text1
+
+print(list) """
+
+
+### Task 6
+
+text = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital."
+
+text1 = text.replace('capital','capital of Germany')
+
+print(text1)
